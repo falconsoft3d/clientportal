27c7e488307f64f1ed9e3a1c91363ccd5753a12a
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-03-12 12:11:22 +0100

    Editar Perfil

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('forgotpassword/', views.forgotpassword, name='forgotpassword'),
     path('logout/', views.logout, name='logout'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('change_password/', views.change_password, name='change_password'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -20,14 +20,18 @@ import requests
 def dashboard(request):
     # orders = Order.objects.order_by('created_at').filter(user_id=request.user.id, is_ordered=True)
     # orders_count = orders.count()
-    # userprofile = UserProfile.objects.get(user_id = request.user.id)
+    userprofile = UserProfile.objects.get(user_id = request.user.id)
     # context = {
     #     'orders' : orders,
     #     'orders_count' : orders_count,
     #     'userprofile' : userprofile,
     # }
     
-    return render(request, 'accounts/dashboard.html')
+    context = {
+        'userprofile' : userprofile,
+    }
+    
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def login(request):
@@ -171,3 +175,30 @@ def change_password(request):
             return redirect('change_password')
         
     return render(request, 'accounts/change_password.html')
+
+
+@login_required
+def edit_profile(request):
+    userprofile = get_object_or_404(UserProfile, user=request.user)
+    # Actualizar o crear perfil
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=userprofile)
+        if user_form.is_valid and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Su información fue guardada con exito')
+            return redirect('edit_profile')
+        
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = UserProfileForm(instance=userprofile)
+        
+    
+    context = {
+        'user_form' : user_form,
+        'profile_form' :  profile_form,
+        'userprofile' : userprofile
+    }
+    
+    return render(request, 'accounts/edit_profile.html', context)
