c715b17fb7cac50ab5a251b8fee4b657310c3e8b
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-03-12 20:19:10 +0100

    sc

--- config/admin.py
+++ config/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import AdminBase
 
 # Register your models here.
+
+# Register your models here.
+class BaseAdminAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone')
+    list_display_link = ('name', 'email', 'phone')
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+    
+    
+admin.site.register(AdminBase, BaseAdminAdmin)
--- config/models.py
+++ config/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class AdminBase(models.Model):
+    name = models.CharField(blank=True, max_length=20)
+    email = models.CharField(blank=True, max_length=20)
+    phone = models.CharField(blank=True, max_length=20)
+    logo = models.ImageField(blank=True, upload_to='logo/')
+    street = models.CharField(blank=True, max_length=20)
+    
+    baner01 = models.ImageField(blank=True, upload_to='baner/')
+    baner02 = models.ImageField(blank=True, upload_to='baner/')
+    baner03 = models.ImageField(blank=True, upload_to='baner/')
+    
+    def __strt__(self):
+        return self.name
