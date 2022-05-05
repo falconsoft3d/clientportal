6c1f52b292079d145f990b2be05f6da145f4318b
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-05-05 22:56:41 +0200

    Agregando documentación con Swagger

--- clientportal/urls.py
+++ clientportal/urls.py
@@ -19,9 +19,26 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import url, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
 
 from apiv1.route import router_api
 handler404 = "clientportal.views.page_not_found_view"
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ClientPortal API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="mfalconsoft@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
 
 urlpatterns = [
     path('secureadmin/', admin.site.urls),
@@ -36,4 +53,10 @@ urlpatterns = [
     path('orders/', include('orders.urls')),
     path('demo/', include('demo.urls')),
     path('apiv1/', include(router_api.urls)),
+
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
